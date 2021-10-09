766cc8be8c98ef9ce71adb18d65ee25c35d5a2d3
Travis Helms <smasher.coder@gmail.com>
2021-10-08 23:17:22 -0500

    password making function done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,39 @@
 // Assignment code here
-
+var symbol = '!"#$%&()*+,-./:;%20<=>?@[\]\'^_`{|}~';
+var number = '0123456789';
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lower = 'abcdefghijklmnopqrstuvwxyz';
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var symbolCheckbox = document.querySelector("input[name=symbols]");
+var lowercaseCheckbox = document.querySelector("input[name=lowercase]");
+var uppercaseCheckbox = document.querySelector("input[name=UPPERCASE]");
+var numberCheckbox = document.querySelector("input[name=numbers]");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = makePass(made, passLength);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
+// Function to create passcode
+function makePass(type, length) {
+  var result = '';
+  var charactersLength = type.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += type.charAt(Math.random() * charactersLength);
+  }
+return result;
+}
+
+
+var passLength = 128;
+var made = symbol + number + upper + lower
+
+
+console.log(makePass(made, passLength));
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
